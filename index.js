34705a4738f7c9219ff56a7e8d1e1ff2b644d7b7
LAGstudi0 <paolo.milutinovic@gmail.com>
2023-12-15 14:57:49 +0000

    changes to the navigation bar

--- index.js
+++ index.js
@@ -6,3 +6,88 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 
 })
+
+
+
+
+
+//     const element = document.createElement('div');
+//     element.classList.add('up-container');
+
+//     //Button for going up the page
+//     const upBtn = document.createElement('a');
+//     upBtn.classList.add('up-btn');
+//     upBtn.href = '#top';
+
+//     window.addEventListener('resize', function() {
+//         if (window.innerWidth <= 600) {
+//             upBtn.href = "#top"; 
+//         }
+//     });
+
+//     if (window.innerWidth <= 600) {
+//         upBtn.href = "#top"; 
+//     }
+
+//     //Image inside the button.
+//     const icon = new Image();
+//     icon.src = "./images/arrowUp.svg";
+
+//     upBtn.appendChild(icon);
+//     element.appendChild(upBtn);
+
+
+
+// document.body.appendChild(element);
+
+
+// below is for the up button
+
+const element = document.createElement('div');
+element.classList.add('up-container');
+
+// Button for going up the page
+const upBtn = document.createElement('a');
+upBtn.classList.add('up-btn');
+
+// Set the href attribute to an empty string to make it a valid anchor link
+upBtn.href = '#';
+
+upBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    // Smooth scroll to the top of the page
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 600) {
+        upBtn.href = '#';
+    }
+});
+
+if (window.innerWidth <= 600) {
+    upBtn.href = '#';
+}
+
+// Image inside the button.
+const icon = new Image();
+icon.src = './images/arrowUp.svg';
+
+upBtn.appendChild(icon);
+element.appendChild(upBtn);
+
+document.body.appendChild(element);
+
+// on top of this is for the up button 
+
+
+
+// this is for the top nav
+window.addEventListener('scroll', () => {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 500);
+});
